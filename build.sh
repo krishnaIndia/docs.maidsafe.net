44c18258404db5954b1c514e0edbf22224825a16
Lionel Faber <lionel1704@gmail.com>
2018-09-11 17:01:32 +0530

    add Whitepapers repo

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-repos=("safe_app_nodejs" "beaker-plugin-safe-app")
+repos=("safe_app_nodejs" "beaker-plugin-safe-app" "Whitepapers")
 for i in "${repos[@]}"; do
   target="dist/$i"
   git clone --depth 1 --branch gh-pages https://github.com/maidsafe/$i $target
