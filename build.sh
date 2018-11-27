319c6945dd5aa21623e357baecbceddd60c721e4
Lionel Faber <lionel1704@gmail.com>
2018-11-27 16:42:13 +0530

    feat/java: Add API documentation for java

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rm -rf dist
-repos=("safe_app_nodejs" "Whitepapers" "safe_app_csharp")
+repos=("safe_app_nodejs" "Whitepapers" "safe_app_csharp" "safe_app_java")
 for i in "${repos[@]}"; do
   target="dist/$i"
   git clone --depth 1 --branch gh-pages https://github.com/maidsafe/$i $target
